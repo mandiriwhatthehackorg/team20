c81e95f81792de7ecd7627542e26547cebf7017f
saidihasan <saidihasan@gmail.com>
2019-07-21 08:16:14 +0700

    little polish

--- app/src/main/java/com/marimo/whatthehack/apps/DetailPengambilan.java
+++ app/src/main/java/com/marimo/whatthehack/apps/DetailPengambilan.java
@@ -1,14 +1,28 @@
 package com.marimo.whatthehack.apps;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailPengambilan extends AppCompatActivity {
+    Button backHome;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_pengambilan);
+        backHome = findViewById(R.id.detail_pengambilan_selesai);
+
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
